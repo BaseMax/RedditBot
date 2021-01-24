8cfc600173b7918b679c236f3f08f67e3beb1432
Max Base <MaxBaseCode@Gmail.Com>
2021-01-24 09:56:44 +0330

    Adding lib and import

--- main.py
+++ main.py
@@ -2,3 +2,17 @@
 # @Author: Max Base
 # @Date: 2021-01-24
 # @Repository: https://github.com/basemax/RedditBot, still private
+
+import os
+import re
+import sys
+import time
+import pickle
+import os.path
+from os import path
+from enum import Enum
+import mysql.connector
+from selenium import webdriver
+from dotenv import load_dotenv, find_dotenv, dotenv_values
+from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import NoSuchElementException
